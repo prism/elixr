1ff1125e5a8b258263a0f7a84ad128dae3765ff2
Mike Botsko <botsko@gmail.com>
2014-08-25 14:41:05 -0700

    Missed file

---
+++ src/main/java/us/dhmc/elixr/commands/arguments/Flag.java
@@ -0,0 +1,13 @@
+package us.dhmc.elixr.commands.arguments;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Flag {
+
+    String[] aliases();
+    
+    boolean acceptsValue() default false;
+    
+}
